e91caede4df8a4c454e981d03176e86b2420d73c
srikanth <chiramanasrikanth265@gmail.com>
2022-05-04 08:07:52 +0530

    create category controller

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
 const connectDB = require('./configs/db');
+const categoryController = require('./controllers/category.controllers');
 const app = express();
 const PORT = 9000;
+
+app.use(express.json());
+app.use('/categories', categoryController);
 
 connectDB();
 
