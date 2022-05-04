d15f16d7f3f2e09523c1adf9ca83aa0b4ed773f0
srikanth <chiramanasrikanth265@gmail.com>
2022-05-04 07:07:18 +0530

    initial commit

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -3,9 +3,6 @@ const express = require('express');
 const app = express();
 const PORT = 9000;
 
-app.get('/test', (req, res) => {
-  res.send('initial test');
-});
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
