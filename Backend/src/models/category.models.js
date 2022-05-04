25d1db783a14d0cf666e43b289b39091d9d87a8b
srikanth <chiramanasrikanth265@gmail.com>
2022-05-04 07:24:37 +0530

    create category model

---
+++ Backend/src/models/category.models.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const categorySchema = mongoose.Schema({
+  name: {
+    type: String,
+  }
+});
+
+module.exports = mongoose.model('Category', categorySchema);
