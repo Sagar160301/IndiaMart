ae410b99673b955ae9d5aec106ba6a70b52313ae
srikanth <chiramanasrikanth265@gmail.com>
2022-05-04 11:12:46 +0530

    change mongodb url to atlas

--- Backend/src/configs/db.js
+++ Backend/src/configs/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+// require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/vali');
+    await mongoose.connect(process.env.MONGO_URI);
   } catch (error) {
     console.log(error);
   }
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const dotnev = require('dotenv');
 
 const connectDB = require('./configs/db');
 const categoryController = require('./controllers/category.controllers');
 const app = express();
 const PORT = 9000;
+
+dotnev.config();
 
 app.use(express.json());
 app.use('/categories', categoryController);
