69fb6f1272f8029411e63041afffc553e3226e81
Ganesh S Prabhu <ganeshsprabhu2000@gmail.com>
2022-05-05 16:44:49 +0530

    login and edit page done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import { Home } from "./pages/Home";
+// import {LoginPage} from "./components/loginComponents/loginpage"
+// import {Edit_profile} from "./components/edit_Profile/edit_profile"
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
+      {/* <LoginPage/> */}
+      {/* <Edit_profile/> */}
     </div>
   );
 }
