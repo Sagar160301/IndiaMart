ff5920c23bb3c463e55b7465118fed702d13f5aa
Tushar Ahire <tusharmadhavahire98@gmail.com>
2022-05-05 20:05:28 +0530

    updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,7 @@
 
 
 import './App.css'
-import {Makeup} from "./pages/Makeup"
-import {Lipstick} from "./pages/Lipstick"
-import {LipsProducts} from "./pages/Lips_Products"
-import {BleachCream} from "./pages/Bleach_Cream"
-import {EyeProduct} from "./pages/Eye_Products"
-import {Lipcare} from "./pages/Lipcare"
-import {Scrub} from "./pages/Scrubs."
+
 
 function App() {
    
@@ -16,19 +10,7 @@ function App() {
     <div className="App">
      
  
-       <Makeup/>
-        <hr></hr>
-        <Lipstick/>
-        <hr></hr>
-        <LipsProducts/>
-        <hr></hr>
-        <BleachCream/>
-          <hr></hr>
-          <Scrub/>
-          <hr></hr>
-        <EyeProduct/>
-        <hr></hr>
-        <Lipcare/>
+  
     </div>
   )
 }
