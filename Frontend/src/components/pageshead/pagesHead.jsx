d92761d68c004fa989ecd73a26fd21aed9d9ac25
Tushar Ahire <tusharmadhavahire98@gmail.com>
2022-05-06 11:28:58 +0530

    updated

---
+++ Frontend/src/components/pageshead/pagesHead.jsx
@@ -0,0 +1,17 @@
+import "./pagesHead.css"
+
+export const Producthead=({productData,type})=>{
+    return <div className="pageMainDiv">
+        <h2 className="pageHead">{type}</h2>
+        <h5 className="pageInfo">{productData.info}</h5>
+         <div className="productDataDiv">
+             {productData.products.map((e)=>{
+                 return <div className="singleProductDiv">
+                     <img className="singleProductImg" src={e.img}/>
+                     <p className="singleProductHead">{e.head}</p>
+                     <p className="singleProductAskPrice">Ask Price</p>
+                 </div>
+             })}
+         </div>
+    </div>
+}
---
+++ Frontend/src/pages/product_pages.jsx
@@ -0,0 +1,26 @@
+import { Producthead } from "../components/pageshead/pagesHead";
+import { Products } from "../components/products/products";
+import { useEffect, useState } from "react";
+import { Sidebar } from "../components/pages_sidebar/pages_sidebar";
+import "./product_pages.css"
+export const Product_pages=({type})=>{
+    const [data,setData]=useState([])
+    useEffect(()=>{
+        getData()
+    },[])
+
+    const getData=async()=>{
+      let res=await fetch("")
+      let FetchedData=await res.json()
+      setData(FetchedData)
+    }
+    return <div className="flexDiv">
+     <div className="flexSidebarDiv">
+        <Sidebar/>
+    </div>
+    <div className="flexmainDiv">
+    <Producthead productData={data} type={type}></Producthead>
+    <Products products={data} type={type}></Products>
+    </div>
+</div>
+ }
